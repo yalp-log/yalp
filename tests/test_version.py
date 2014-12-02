e93f19d35993c2f76944caa7ca33f64ae8afdc07
Timothy Messier <tim.messier@gmail.com>
2014-12-02 00:36:18 -0500

    Add initial tests

---
+++ tests/test_version.py
@@ -0,0 +1,14 @@
+# vim: set et ts=4 sw=4 fileencoding=utf-8:
+'''
+tests.test_version
+==================
+'''
+import unittest
+
+class TestVersion(unittest.TestCase):
+    '''
+    Test getting version
+    '''
+    def test_get_version(self):
+        from yalp import version
+        self.assertIsNotNone(version.__version__)
