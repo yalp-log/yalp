9451498a57161c240d56228d61fb7dcea32d576f
Timothy Messier <tim.messier@gmail.com>
2014-12-02 00:21:02 -0500

    Add tox

--- yalp/version.py
+++ yalp/version.py
@@ -5,10 +5,11 @@ yalp.version
 
 Get version of yalp
 '''
+from __future__ import print_function
 
 from distutils.version import StrictVersion  # pylint: disable=E0611,F0401
 __version__ = str(StrictVersion('0.1a1'))
 del StrictVersion
 
 if __name__ == '__main__':
-    print __version__
+    print(__version__)
