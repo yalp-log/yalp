76cba0a5cc1ecaa5cae451caa3e2b395ade63ac5
Timothy Messier <tim.messier@gmail.com>
2014-12-12 23:00:51 -0500

    More get_yalp_class test cases

--- tests/test_get_yalp_class.py
+++ tests/test_get_yalp_class.py
@@ -4,7 +4,9 @@ tests.test_get_yalp_class
 =========================
 '''
 import unittest
+from nose.tools import raises
 from yalp.utils import get_yalp_class
+from yalp.exceptions import ImproperlyConfigured
 
 
 class TestGetYalpClass(unittest.TestCase):
@@ -39,3 +41,23 @@ class TestGetYalpClass(unittest.TestCase):
         instance = get_yalp_class(plugin, config, plugin_type)
         from yalp.outputs.stdout import Outputer
         self.assertTrue(isinstance(instance, Outputer))
+
+    def test_get_parser_class_type(self):
+        ''' test getting parser class with type '''
+        plugin_type = 'parser'
+        plugin = 'passthrough'
+        config = {
+            'type': 'passthrough',
+        }
+        instance = get_yalp_class(plugin, config, plugin_type)
+        from yalp.parsers.passthrough import Parser
+        self.assertTrue(isinstance(instance, Parser))
+        self.assertEqual(instance.type_, 'passthrough')
+
+    @raises(ImproperlyConfigured)
+    def test_invalid_config(self):
+        ''' test that exception is raised on no valid configs '''
+        plugin_type = 'parser'
+        plugin = 'no_such_plugin'
+        config = {}
+        get_yalp_class(plugin, config, plugin_type)
