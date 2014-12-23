8b42aef77b8dfe69ccdc355bc8cfa08e9f20202d
Timothy Messier <tim.messier@gmail.com>
2014-12-22 21:02:21 -0500

    Revert "remove test util code"
    This reverts commit fd3db37f2ec7728edaba5612b9a5da1dbe61d606.

---
+++ yalp/test/__init__.py
@@ -0,0 +1,12 @@
+# vim: set et ts=4 sw=4 fileencoding=utf-8:
+'''
+yalp.test
+=========
+'''
+from yalp.test.testcase import YalpTestCase
+from yalp.test.utils import override_settings
+
+
+__all__ = [
+    'YalpTestCase', 'override_settings'
+]
