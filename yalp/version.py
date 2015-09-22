7c108a9b7c6c45d1dadb537b67dd1998cd9c50be
Timothy Messier <tim.messier@gmail.com>
2015-09-22 14:51:56 -0400

    prep release

--- yalp/version.py
+++ yalp/version.py
@@ -8,7 +8,7 @@ Get version of yalp
 from __future__ import print_function
 
 from distutils.version import StrictVersion  # pylint: disable=E0611,F0401
-__version__ = str(StrictVersion('1.1'))
+__version__ = str(StrictVersion('1.2'))
 del StrictVersion
 
 if __name__ == '__main__':  # pragma: no cover
